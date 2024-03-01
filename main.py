90d1a812b197ff8acb2a6e20111dca3ca61e2ff0
rmso27 <rubenmsoliveira@gmail.com>
2024-03-01 12:34:26 +0000

    final commit

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 # Import modules
 import sys
-from src.modules.sync.sync_func import sync_process
-
-# Read arguments
-src_folder_path = sys.argv[1]
-dest_folder_path = sys.argv[2]
-sync_interval = int(sys.argv[3])
-logs_path = sys.argv[4]
 
 # Start script
 if __name__ == '__main__':
-    sync_process(src_folder_path, dest_folder_path, sync_interval)
-
-'''
-    TODO:
-    - Update README;
-'''
+    if len(sys.argv) == 5:
+        from src.modules.sync.sync_func import sync_process
+        sync_process(sys.argv[1], sys.argv[2], int(sys.argv[3]))
+    else:
+        print("The script expects 4 arguments: <SOURCE_FOLDER> <DEST_FOLDER> <SYNC_INTERVAL> <LOGS_PATH>")
