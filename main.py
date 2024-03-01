39d5f281b220319cad4f5dc881b34035ece953e1
rmso27 <rubenmsoliveira@gmail.com>
2024-03-01 08:49:28 +0000

    read args from cmd line

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 # Import modules
+import sys
 from src.modules.sync.sync_func import sync_process
 
 # Read arguments
-src_folder_path = "/home/ruben/Documents/repos/veeam-test-task/test_env/src"
-dest_folder_path = "/home/ruben/Documents/repos/veeam-test-task/test_env/dst"
-sync_interval = 10
-logs_path = ""
+src_folder_path = sys.argv[1]
+dest_folder_path = sys.argv[2]
+sync_interval = int(sys.argv[3])
+logs_path = sys.argv[4]
 
-
+# Start script
 if __name__ == '__main__':
     sync_process(src_folder_path, dest_folder_path, sync_interval)
 
 '''
     TODO:
-    - Update folder deletion to validate if content of folder is equal to source;
-    - Update script to read arguments provided by the user;
-    - Update comments;
     - Update README;
 '''
