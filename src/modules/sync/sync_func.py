23ec21631276dd0d17112ac6984fc943633a8506
rmso27 <rubenmsoliveira@gmail.com>
2024-03-01 08:49:11 +0000

    split the of folders and files into seperate functions

--- src/modules/sync/sync_func.py
+++ src/modules/sync/sync_func.py
@@ -1,3 +1,11 @@
+"""
+    DESCRIPTION:
+    In this file I've set the all of the needed synced funtions.
+    The 'sync_process' function continuously synchronizes files and folders between a source and
+    destination directory at a specified interval, handling files and folders separately with
+    appropriate logging and error handling.
+"""
+
 # Import modules
 import os
 import shutil
@@ -26,37 +34,54 @@ def sync_process(src_folder, dest_folder, sync_interval):
 
                 # Validate if it's a file or a folder
                 if os.path.isfile(source_item):
-                    # Files
-
-                    # If file exists in destination, delete it
-                    if os.path.exists(destination_item):
-                        try:
-                            os.remove(destination_item)
-                            logging.warning(f"File '{item_name}' already existed and it was DELETED FROM {destination_item}.")
-                        except:
-                            logging.error(f"File '{item_name}' already exists and it was not possible to delete it.")
-                    try:
-                        shutil.copy2(source_item, destination_item)
-                        logging.info(f"File '{item_name}' synced successfully FROM {source_item} TO {destination_item}.")
-                    except:
-                        logging.error(f"FAILED to copy file '{item_name}'.")
+                    sync_file(item_name, source_item, destination_item)
                 else:
-                    # Folders
-
-                    # If folder already exists in destination, delete it
-                    if os.path.exists(destination_item):
-                        try:
-                            shutil.rmtree(destination_item)
-                            logging.warning(f"Folder '{item_name}' already existed and it was DELETED FROM {destination_item}.")
-                        except:
-                            logging.error(f"Folder '{item_name}' already exists and it was not possible to delete it.")
-                    try:
-                        shutil.copytree(source_item, destination_item)
-                        logging.info(f"Folder '{item_name}' synced successfully FROM {source_item} TO {destination_item}.")
-                    except:
-                        logging.error(f"FAILED to copy folder '{item_name}'.")
+                    sync_folder(item_name, source_item, destination_item)
         except Exception as ex:
-            print(f"Error while executing synchronization: {ex}")
+            logging.error(f"Error while executing synchronization: {ex}")
 
         # Wait specified sync interval
         time.sleep(sync_interval)
+
+'''
+    NOTE:
+    I've segregated the sync between folders / files in 2 different functions because they are handled differently.
+'''
+
+# Folder sync
+def sync_folder(item_name, source_item, destination_item):
+
+    '''
+        NOTE:
+        In this function, the first thing that I do is to delete the folder if it exists. I'm doing this because in the requirements
+        it says that the content of the DESTINY should be the same as the SOURCE.
+    '''
+
+    # If folder already exists in destination, delete it
+    if os.path.exists(destination_item):
+        try:
+            shutil.rmtree(destination_item)
+            logging.warning(f"Folder '{item_name}' already existed and it was DELETED FROM {destination_item}.")
+        except:
+            logging.error(f"Folder '{item_name}' already exists and it was not possible to delete it.")
+    try:
+        shutil.copytree(source_item, destination_item)
+        logging.info(f"Folder '{item_name}' synced successfully FROM {source_item} TO {destination_item}.")
+    except:
+        logging.error(f"FAILED to copy folder '{item_name}'.")
+
+# File sync
+def sync_file(item_name, source_item, destination_item):
+
+    # If file exists in destination, delete it
+    if os.path.exists(destination_item):
+        try:
+            os.remove(destination_item)
+            logging.warning(f"File '{item_name}' already existed and it was DELETED FROM {destination_item}.")
+        except:
+            logging.error(f"File '{item_name}' already exists and it was not possible to delete it.")
+    try:
+        shutil.copy2(source_item, destination_item)
+        logging.info(f"File '{item_name}' synced successfully FROM {source_item} TO {destination_item}.")
+    except:
+        logging.error(f"FAILED to copy file '{item_name}'.")
